806b3581a3fb85b2b9087e725efec5c386fd7419
Ambroise Maire <ambroise.maire7@gmail.com>
2023-08-09 14:25:37 +0200

    chore: Classes and DOMElements
    Created classes in 'elements.js' for creating multiple elements in the DOM with javascript.
    
    Created the header and almost finished the core of the page with this system.

--- src/assets/scripts/generation/elements.js
+++ src/assets/scripts/generation/elements.js
@@ -1,25 +1,67 @@
 //=-=-=-=-=-Header-=-=-=-=-=//
-export class SvgElement {
-    constructor(id, width, height, path) {
-        this.element = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-        this.element.id = id;
-        this.element.style.width = width;
-        this.element.style.height = height;
-        this.element.innerHTML = path;
-    }
-
+class BaseElement {
     appendTo(parent) {
         parent.appendChild(this.element)
+    }
+    cloneInto(parent) {
+        parent.appendChild(this.element.cloneNode(true));
+    }
+}
+
+export class SvgElement extends BaseElement {
+    constructor(id = '', classe = '', width, height, path, fill = '#000000') {
+        super();
+        this.element = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        this.element.id = id;
+        if (classe !== null && classe !== '') { this.element.classList.add(classe) }
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${height}px`;
+        this.element.setAttribute('fill', fill);
+        this.element.innerHTML = path;
     }
 }
 
 //=-=-=-=-=-Page Core-=-=-=-=-=//
-export class DivElement {
-    constructor(id) {
+export class TaskTemplate extends BaseElement {
+    constructor() {
+        super();
+        this.element = document.createElement('template');
+    }
+}
+
+export class DivElement extends BaseElement {
+    constructor(id = '', classe = '') {
+        super();
         this.element = document.createElement('div');
         this.element.id = id;
+        if (classe !== null && classe !== '') { this.element.classList.add(classe) };
     }
-    appendTo(parent) {
-        parent.appendChild(this.element)
+}
+
+export class InputElement extends BaseElement {
+    constructor(id = '') {
+        super();
+        this.element = document.createElement('input');
+        this.element.name = id;
+    }
+}
+
+export class ButtonElement extends BaseElement {
+    constructor(id = '', classe = '', type = '', content = '') {
+        super();
+        this.element = document.createElement('button');
+        this.element.id = id;
+        if (classe !== null && classe !== '') { this.element.classList.add(classe) };
+        this.element.type = type;
+        this.element.innerHTML = content;
+    }
+}
+
+export class Paragraph extends BaseElement {
+    constructor(id = '', text = '') {
+        super();
+        this.element = document.createElement('p');
+        this.element.id = id;
+        this.element.innerText = text;
     }
 }
