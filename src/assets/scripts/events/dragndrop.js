245038fbaca840527620100bdd4701ea75fc5674
Ambroise Maire <ambroise.maire7@gmail.com>
2023-08-31 16:53:13 +0200

    chore: Mobile Drag and Drop (remastered lol)
    <JavaScript>
    dragndrop.js : Intergration of the mobile browsers' version of Drag and Drop (TouchEvent) in order to make it work on them. Tasks can be dragged, swaping tasks function (touchend event) is in progress.

--- src/assets/scripts/events/dragndrop.js
+++ src/assets/scripts/events/dragndrop.js
@@ -1,4 +1,4 @@
-import { storeData } from './functions';
+import { instanceTemplate, storeData } from './functions';
 
 //=-=-=-=-=-=-=-Drag and Drop-=-=-=-=-=-=-=//
 /**
@@ -16,15 +16,16 @@ export function dragndrop() {
         taskbox.insertBefore(obj2, temp);
         taskbox.removeChild(temp);
     };
-    
+
+    // Desktop Drag and Drop //    
     draggables.forEach(draggable => {
 
-        draggable.addEventListener('drag', (e) => {
+        draggable.addEventListener('drag', e => {
             const dragged = e.target;
             dragged.classList.add('dragging');
         });
         
-        draggable.addEventListener('drop', (e) => {
+        draggable.addEventListener('drop', e => {
             e.preventDefault();
             const target = e.target;
             const dragged = document.querySelector('.dragging');
@@ -46,12 +47,12 @@ export function dragndrop() {
             };
         });
     
-        draggable.addEventListener('dragend', (e) => {
+        draggable.addEventListener('dragend', e => {
             const dragged = e.target;
             dragged.classList.remove('dragging');
         });
     
-        draggable.addEventListener('dragover', (e) => {
+        draggable.addEventListener('dragover', e => {
             e.preventDefault();
             const target = e.target;
             const dragged = document.querySelector('.dragging');
@@ -67,7 +68,7 @@ export function dragndrop() {
             
         });
     
-        draggable.addEventListener('dragleave', (e) => {
+        draggable.addEventListener('dragleave', e => {
             e.preventDefault();
             const targetting = e.target;
             if (targetting.nodeName == 'P') {
@@ -76,5 +77,79 @@ export function dragndrop() {
                 targetting.classList.remove('targetting');
             }
         });
+    });
+    // window.addEventListener('touchmove', e => {
+    //     const touch = e.touches[0];
+    //     return touch;
+    // });
+
+    // Mobile Drag and Drop //
+    draggables.forEach(touchable => {
+        const taskbox = document.querySelector('#tasks-box');
+        var location, touchTarget;
+
+        touchable.addEventListener('touchstart', e => {
+            location = e.touches[0];
+            const touching = e.target;
+
+            const temp = touching.cloneNode(true);
+            temp.id = 'temp';
+            temp.style.opacity = '0.5';
+            taskbox.insertBefore(temp, touching);
+
+            touching.classList.add('touching');
+            touching.style.top = `calc(${location.clientY}px - 5vh)`;
+            touching.style.left = `calc(${location.clientX}px - 40vw)`;
+
+            e.stopImmediatePropagation();
+        });
+
+        touchable.addEventListener('touchmove', e => {
+            e.preventDefault();
+            location = e.touches[0];
+
+            touchTarget = document.elementFromPoint(location.clientX, location.clientY);
+            console.log(location.clientX, location.clientY);
+            const touching = e.target;
+            touching.style.top = `calc(${location.clientY}px - 5vh)`;
+            touching.style.left = `calc(${location.clientX}px - 40vw)`;
+            e.stopImmediatePropagation();
+        });
+
+        touchable.addEventListener('touchend', e => {
+            const touching = e.target;
+            console.log(location);
+            if (touchTarget) {
+                if (touchTarget.classList.contains('task')) {
+                    swapElements(touchTarget, touching);
+                };
+            }
+            touching.classList.remove('touching');
+            touching.style ='';
+
+            const temp = document.querySelector('#temp');
+            if (temp) {
+                temp.remove();
+            }
+        });
+
+        /*
+        "FIX MEEEE"
+         - James Hetfield
+        */
+        // touchable.addEventListener('touchleave', e => {
+        //     const touching = e.target;
+
+        //     taskbox.style = '';
+        //     touching.style = '';
+        //     touching.classList.remove('touching');
+
+        //     const temp = document.querySelector('#temp');
+        //     if (temp) {
+        //         temp.remove();
+        //     }
+
+        //     console.log('coucou')
+        // })
     });
 };
